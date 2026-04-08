0b6b24eee5bf039dbd7ecdd96e38cb676d5b1d74
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-04-07 21:21:49 -0300

    Update 04_exploratory_analysis.sql

--- sql/04_exploratory_analysis.sql
+++ sql/04_exploratory_analysis.sql
@@ -1,1 +1,5 @@
-
+SELECT
+  CORR(age, total_emprestimos) AS corr_idade_emprestimos,
+  CORR(salario_tratado, total_emprestimos) AS corr_salario_emprestimos,
+  CORR(debt_ratio, default_flag) AS corr_endividamento_default
+FROM `view-tab-unica`;
