da5b4e91c281303342a8ffd86670e7a165481c56
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-04-07 21:22:42 -0300

    Update 01_data_cleaning.sql

--- sql/01_data_cleaning.sql
+++ sql/01_data_cleaning.sql
@@ -10,4 +10,4 @@ FROM `risco-relativo-460118.dadosbancocaja.user_info` AS u
 
 -- Remove registros sem user_id
 INNER JOIN `risco-relativo-460118.dadosbancocaja.loans_outstanding` AS loan
-ON u.user_id = loan.user_id
+ON u.user_id = loan.user_id;
