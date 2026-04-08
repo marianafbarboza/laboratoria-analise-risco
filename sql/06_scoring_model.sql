7fb5b12e8d193faea14df332acda33cd0c632d60
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-04-07 21:27:08 -0300

    Update 06_scoring_model.sql

--- sql/06_scoring_model.sql
+++ sql/06_scoring_model.sql
@@ -1,1 +1,33 @@
+-- Score final por cliente
 
+SELECT
+  user_id,
+  SUM(peso) AS score_risco
+FROM quartis_empilhados q
+JOIN pesos_risco p
+  ON q.variavel = p.variavel AND q.faixa = p.faixa
+GROUP BY user_id;
+
+
+
+-- Score final por cliente
+
+SELECT
+  user_id,
+  SUM(peso) AS score_risco
+FROM quartis_empilhados q
+JOIN pesos_risco p
+  ON q.variavel = p.variavel AND q.faixa = p.faixa
+GROUP BY user_id;
+
+
+
+-- Classificação de risco
+
+SELECT *,
+  CASE
+    WHEN score_risco > 22 THEN 'alto_risco'
+    WHEN score_risco BETWEEN 18 AND 22 THEN 'medio_risco'
+    ELSE 'baixo_risco'
+  END AS faixa_risco
+FROM pontuacao_risco;
