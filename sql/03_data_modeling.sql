c558f4d953605d34e356ff2a89917b5666793f0b
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-04-07 21:21:27 -0300

    Update 03_data_modeling.sql

--- sql/03_data_modeling.sql
+++ sql/03_data_modeling.sql
@@ -1,1 +1,22 @@
+-- View principal
 
+SELECT
+  loan.user_id,
+  u.age,
+  u.salario_tratado,
+  u.dependentes_tratado,
+  loan.qtde_real_estate,
+  loan.qtde_other,
+  loan.total_emprestimos,
+  ld.using_lines_not_secured_personal_assets,
+  ld.number_times_delayed_payment_loan_30_59_days,
+  ld.debt_ratio,
+  COALESCE(d.default_flag, 0) AS default_flag
+
+FROM `view-user-info` u
+INNER JOIN `view-novas-variaveis` loan
+  ON u.user_id = loan.user_id
+LEFT JOIN `view-loans_details` ld
+  ON loan.user_id = ld.user_id
+LEFT JOIN `default` d
+  ON loan.user_id = d.user_id
