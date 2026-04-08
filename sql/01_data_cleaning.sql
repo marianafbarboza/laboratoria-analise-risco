c511edc43b526acc2e167183b0a1b0038441fff5
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-04-07 21:12:22 -0300

    Update 01_data_cleaning.sql

--- sql/01_data_cleaning.sql
+++ sql/01_data_cleaning.sql
@@ -1,1 +1,13 @@
+-- Tratamento de valores nulos + base inicial
 
+SELECT
+  DISTINCT loan.user_id,
+  u.age,
+  COALESCE(u.last_month_salary, 0) AS salario_tratado,
+  COALESCE(u.number_dependents, 0) AS dependentes_tratado
+
+FROM `risco-relativo-460118.dadosbancocaja.user_info` AS u
+
+-- Remove registros sem user_id
+INNER JOIN `risco-relativo-460118.dadosbancocaja.loans_outstanding` AS loan
+ON u.user_id = loan.user_id
