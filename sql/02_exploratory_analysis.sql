e0be5a30426da88dc6054fbafc032a394f741287
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-04-07 21:20:37 -0300

    Update 02_exploratory_analysis.sql

--- sql/02_exploratory_analysis.sql
+++ sql/02_exploratory_analysis.sql
@@ -1,1 +1,18 @@
+-- Padronização de loan_type
+SELECT
+  user_id,
+  CASE
+    WHEN LOWER(loan_type) LIKE '%real%' THEN 'real_estate'
+    ELSE 'other'
+  END AS loan_type_padronizado
+FROM `risco-relativo-460118.dadosbancocaja.loans_outstanding`
 
+  
+-- Agregação de empréstimos por cliente
+SELECT
+  user_id,
+  COUNTIF(loan_type = 'real_estate') AS qtde_real_estate,
+  COUNTIF(loan_type != 'real_estate') AS qtde_other,
+  COUNT(*) AS total_emprestimos
+FROM `risco-relativo-460118.dadosbancocaja.loans_outstanding`
+GROUP BY user_id
